79a4527147c14138d3ebfb4337689f5495827aaa
EssenceOfChaos <fjschiller@gmail.com>
2016-12-04 23:46:03 -0500

    paperclip gem- added validation to model, whitelisted attribute in controller, and added file upload in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	has_many :tasks
+	 has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205041936) do
+ActiveRecord::Schema.define(version: 20161205043601) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(version: 20161205041936) do
     t.string   "display_name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
